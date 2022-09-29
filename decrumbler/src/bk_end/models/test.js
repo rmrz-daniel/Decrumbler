c40926d22a0e5704440b78e17b39eea0f3108966
Daniel Ramirez <daniel.ramirez@edu.sait.ca>
2022-09-29 15:58:08 -0600

    Update to the schema
    Includes all the fields given to us from the sample data

--- decrumbler/src/bk_end/models/test.js
+++ decrumbler/src/bk_end/models/test.js
@@ -2,10 +2,34 @@ const mongoose = require('mongoose');
 
 function dynamicSchema(collection){
     let dynamicColl = new mongoose.Schema({
-        cookies: {
+        Name: {
             type: String,
             required: true
-        }
+        },
+        Subnet: {
+            type: String,
+            required: true
+        },
+        VLAN: {
+            type: String,
+            required: true
+        },
+        state: {
+            type: String,
+            required: true
+        },
+        GW: {
+            type: String,
+            required: true
+        },
+        VRF: {
+            type: String,
+            required: true
+        },
+        Location: {
+            type: String,
+            required: true
+        },
     },
     {
         collection: collection
