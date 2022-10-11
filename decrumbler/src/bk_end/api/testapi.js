2315566816b6e130e95ff835cba4b4adeac51ad6
Daniel Ramirez <daniel.ramirez@edu.sait.ca>
2022-10-11 16:37:06 -0600

    Backend updates
    Adds GET and PUT requests to api
    Changes start to open front-end and back-end together using Concurrently
    Adds Concurrently package

--- decrumbler/src/bk_end/api/testapi.js
+++ decrumbler/src/bk_end/api/testapi.js
@@ -14,5 +14,38 @@ router.post('/test/:collectionName',
     }
 );
 
+router.get('/test/:collectionName',
+    function (req, res, next) {
+
+        var coll = require('../models/test')(req.params.collectionName);
+       
+        coll.find({}).then(
+            function (collection) {
+                if (collection != null) {
+                    res.send(collection);
+                } else {
+                    res.status(404).send('Sorry can\'t find that!')
+                }
+            }
+        ).catch(next);
+    }
+);
+
+router.put('/test/:collectionName/:_id',
+    function (req, res, next) {
+        var coll = require('../models/test')(req.params.collectionName);
+
+        coll.findOneAndUpdate({ _id: req.params._id }, req.body).then(
+            function (subnet) {
+                coll.findOne({ _id: req.params._id }).then(
+                    function (subnet) {
+                        res.send(subnet)
+                    }
+                )
+            }
+        )
+    }
+)
+
 module.exports = router;
 
