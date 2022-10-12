82b158149046ce719115cf4d52dc32e9ee959c73
Leila <leila.kassi@outlook.com>
2022-10-12 11:54:12 -0600

    change reactdom render to create root

--- decrumbler/src/components/mainView/displaySubnet.js
+++ decrumbler/src/components/mainView/displaySubnet.js
@@ -6,7 +6,7 @@ const DisplaySubnet = (props) => {
     //Fetch data from database
     useEffect(() => {
         async function getSubnets() {
-            const response = await fetch("http://localhost:3000/testapi"); //find port server is hosted on
+            const response = await fetch("http://localhost:4444/api/test/02-10"); //find port server is hosted on
 
             if(!response.ok) {
                 const errMessage = "Sorry, something went wrong";
@@ -28,22 +28,7 @@ const DisplaySubnet = (props) => {
         <li className="list">
             <div id="subnet">
                 <div>
-                    {props.testapi.Name}
-                </div>
-                <div>
-                    {props.testapi.Subnet}
-                </div>
-                <div>
-                    {props.testapi.VLAN}
-                </div>
-                <div>
-                    {props.testapi.state}
-                </div>
-                <div>
-                    {props.testapi.GW}
-                </div>
-                <div>
-                    {props.testapi.Location}
+                    {props.testapi.collection}
                 </div>
             </div>
         </li>
