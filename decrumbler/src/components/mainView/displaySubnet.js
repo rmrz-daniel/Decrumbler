43868f0495d3beb42b74b69e7eae58dc53748544
Daniel Ramirez <daniel.ramirez@edu.sait.ca>
2022-11-02 14:09:27 -0600

    Update displaySubnet.js
    Basic fetching

--- decrumbler/src/components/mainView/displaySubnet.js
+++ decrumbler/src/components/mainView/displaySubnet.js
@@ -1,6 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, {useState, useEffect} from "react";
 
 function DisplaySubnet() {
+
+    const [subnet, setSubnet] = useState([]);
+
+    useEffect( function(){
+        axios.get('http://localhost:4444/api/test/02-10')
+            .then(
+                function({data}){
+                    setSubnet(data);
+                }
+            ).catch(
+                function(error){
+                    console.log(error)
+                }
+            )
+        },[]
+    )
+
     return (
         <div className='bg-cookie-white'>
             <div className='flex flex-row h-screen '>
@@ -22,7 +40,7 @@ function DisplaySubnet() {
 
                     </div>
                     <div className="flex flex-col w-full h-full 2xl:w-11/12 border rounded-t-lg border-cookie-brown items-center">
-                        <div className="flex flex-row w-full bg-cookie-hazel/70 rounded-t-lg h-10 items-center justify-start text-cookie-brown">
+                        {/* <div className="flex flex-row w-full bg-cookie-hazel/70 rounded-t-lg h-10 items-center justify-start text-cookie-brown">
                             <p className="px-14 font-medium text-xl">Name</p>
                             <p className="px-14 font-medium text-xl">Subnet</p>
                             <p className="px-14 font-medium text-xl">VLAN</p>
@@ -32,6 +50,52 @@ function DisplaySubnet() {
                             <p className="px-14 font-medium text-xl">Status</p>
                             <p className="px-14 font-medium text-xl">Last Pinged</p>
                         </div>
+                        {subnet.map(
+                            function(res){
+                                return(
+                                    <div className="flex flex-row w-full h-10 items-center justify-start text-cookie-brown">
+                                        <p className="px-14">{res.Name}</p>
+                                        <p className="px-14">Subnet</p>
+                                        <p className="px-14">VLAN</p>
+                                        <p className="px-14">VRF</p>
+                                        <p className="px-14">GW</p>
+                                        <p className="px-14">Location</p>
+                                        <p className="px-14">Status</p>
+                                        <p className="px-14">Last Pinged</p>
+                                    </div>
+                                )    
+                            }
+                        )} */}
+                        <table className="table-fixed w-full content-center">
+                            <thead className="bg-cookie-hazel/70 h-10 items-center justify-start text-cookie-brown">
+                                <th>Name</th>
+                                <th>Subnet</th>
+                                <th>VLAN</th>
+                                <th>VRF</th>
+                                <th>GW</th>
+                                <th>Location</th>
+                                <th>Status</th>
+                                <th>Last Pinged</th>
+                            </thead>
+                            <tbody className="mx-auto">
+                                {subnet.map(
+                                    function(res){
+                                        return(
+                                            <tr>
+                                                <td >{res.Name}</td>
+                                                <td>{res.Subnet}</td>
+                                                <td>{res.VLAN}</td>
+                                                <td>{res.VRF}</td>
+                                                <td>{res.GW}</td>
+                                                <td>{res.Location}</td>
+                                                <td>{res.State}</td>
+                                                <td>N/A</td>
+                                            </tr>
+                                        )    
+                                    }
+                                )}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
