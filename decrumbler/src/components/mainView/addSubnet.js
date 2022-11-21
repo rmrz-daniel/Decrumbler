2958eab0ee3020013f1e6b6d5cdd07fad5205011
Daniel Ramirez <daniel.ramirez@edu.sait.ca>
2022-11-21 01:48:04 -0700

    Mass delete :grimacing:
    Just deleting everything

--- decrumbler/src/components/mainView/addSubnet.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function AddSubnet() {
-  return (
-    <div>create the stuff here</div>
-  )
-}
-
-export default AddSubnet
--- decrumbler/src/components/sideBar/subnetList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SubnetList() {
-  return (
-    <div>subnetList</div>
-  )
-}
-
-export default SubnetList
